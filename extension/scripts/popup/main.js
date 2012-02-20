d563bf799767d8550af047707ca07214accb1a7e
James Newbery <james@tinnedfruit.com>
2012-02-20 21:45:19 +0000

    Move extension files into dedicated folder

---
+++ extension/scripts/popup/main.js
@@ -0,0 +1,11 @@
+(function(window, document, $, chrome) {
+	
+	var init = function() {
+		var bgPage = chrome.extension.getBackgroundPage(),
+			app = new App(bgPage);
+			app.init();
+	};
+
+	$(init);
+		
+})(this, this.document, jQuery, this.chrome);
