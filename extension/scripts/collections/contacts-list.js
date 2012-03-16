6b7afe4c0b84e5031b4fdfde8e8ef00eabe27898
James Newbery <james@tinnedfruit.com>
2012-03-16 09:30:10 +0000

    Next steps for loading

--- extension/scripts/collections/contacts-list.js
+++ extension/scripts/collections/contacts-list.js
@@ -9,5 +9,12 @@ var ContactsList = Backbone.Collection.extend({
 
   handleUserIdReset: function() {
     console.log('start pulling actual contacts', this.userIds.get('ids'));
+    // divide userIds into 100 user chunks
+    // create a new 'UserLookup' object for each (collection?)
+    // fetch each collection
+    // when each returns, add to this master contact list
+    // show loading status / count / stats as they are returned
+    // show full list when all complete
+    // store in local storage when all complete
   }
 });
